1580133155e9f6ead1ab23d3ff76946e76f4b240
Nick Ramirez <nickram44@hotmail.com>
2019-07-11 12:22:50 -0400

    Changed cat photo

--- katacodacomnramirezlearn-haproxy/intro-coursedata.sh
+++ katacodacomnramirezlearn-haproxy/intro-coursedata.sh
@@ -5,4 +5,4 @@ docker-compose up -d
 
 apt install -y netcat
 
-wget -O cat.png https://cdn.pixabay.com/photo/2018/03/31/03/46/kitten-3277268_960_720.png
+wget -O cat.png https://cdn.pixabay.com/photo/2016/11/09/23/16/music-1813100_960_720.png
